0d4037f7e609e428e9b3b52edc86f5dd516ff0e1
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-06-24 21:48:24 -0400

    Fix submit button too correctly call something(), separate js from html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDkwICpMk8dgqUwGKbaLCISVcnq6hsnTcc",
+    authDomain: "phone2word.firebaseapp.com",
+    databaseURL: "https://phone2word.firebaseio.com",
+    projectId: "phone2word",
+    storageBucket: "phone2word.appspot.com",
+    messagingSenderId: "989899451141",
+    appId: "1:989899451141:web:a8186cbdfad2f42896656c",
+    measurementId: "G-ZBR9BQFTXW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+
+function something() {
+    event.preventDefault();
+    console.log("begin");
+    var leadsRef = database.ref('mappings');
+    leadsRef.on('value', function(snapshot) {
+         snapshot.forEach(function(childSnapshot) {
+         var childData = snapshot.node_.children_.root_.value.value_;
+         console.log("snapshot.node_.children_.root_.value.value_: ", snapshot.node_.children_.root_.value.value_)
+        });
+    });
+}
+
+
+document.getElementById("Submit").addEventListener("click", something, false);
